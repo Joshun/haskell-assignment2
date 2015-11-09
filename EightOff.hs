24affde6119a1e6f1078f1983499f0970d6093c9
Joshua O'Leary <joshua.oleary@btinternet.com>
2015-11-09 16:45:09 +0000

    Add utility functions isAce, isKing, pCard, sCard

--- EightOff.hs
+++ EightOff.hs
@@ -1,10 +1,10 @@
 module EightOff where
-  data Suit = Heart | Diamond | Spades | Clubs deriving (Enum, Show)
-  data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | King | Queen deriving (Enum, Show)
+  data Suit = Heart | Diamond | Spades | Clubs deriving (Eq, Enum, Show)
+  data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | King | Queen deriving (Eq, Enum, Show)
   type Card = (Suit,Rank)
   type Deck = [Card]
 
-  data Foundation = Card (Foundation)
+  -- data Foundation = Card (Foundation)
 
   -- newDeck :: [Deck]
   -- newDeck = map (\suit -> rank -> (suit,rank)) [Heart .. Clubs] [Ace .. Queen]
@@ -18,3 +18,17 @@ module EightOff where
 
   pipValue :: Card -> Int
   pipValue (suit,rank) = fromEnum rank
+
+  sCard :: Card -> Card
+  sCard (suit,Queen) = (suit,Ace)
+  sCard (suit,rank) = (suit,succ rank)
+
+  pCard :: Card -> Card
+  pCard (suit,Ace) = (suit,Queen)
+  pCard (suit,rank) = (suit, pred rank)
+
+  isAce :: Card -> Bool
+  isAce (suit,rank) = rank == Ace
+
+  isKing :: Card -> Bool
+  isKing (suit,rank) = rank == King
